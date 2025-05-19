463a72ecab361f741b20e43610d47d4b24902c1f
Isabella1807 <megaoreomuffin@hotmail.com>
2025-05-19 22:28:15 +0200

    wip

---
+++ js/components.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+    const container = document.getElementById('content');
+    container.innerHTML = `
+        <nav>
+            <div>
+                <a href="/index.html">Home</a>
+                <a href="/pages/tester.html">Tester</a>
+            </div>
+        </nav>`;
+};
